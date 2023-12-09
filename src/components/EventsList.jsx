35ee505d09335a2cb9a03643644d987a7cbc58bf
Pirvulescu Miruna <pirvulescu03miruna@yahoo.com>
2023-12-10 01:44:05 +0200

    miru

--- src/components/EventsList.jsx
+++ src/components/EventsList.jsx
@@ -1,7 +1,6 @@
-// EventsList.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Slider from 'react-slick';
 import {
-  List,
   ListItem,
   ListItemText,
   ListItemAvatar,
@@ -9,12 +8,28 @@ import {
   Avatar,
   Typography,
   Paper,
+  Chip,
 } from '@mui/material';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 
 function EventsList() {
   const [likedEvents, setLikedEvents] = useState([]);
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    // Fetch events from backend using fetch
+    fetch('http://localhost:8080/events')
+      .then(response => response.json())
+      .then(data => {
+        console.log("Fetched data:", data);
+        setEvents(data);
+      })
+      .catch(error => {
+        console.error('Error fetching events:', error);
+      });
+  }, []);
 
   const handleLikeClick = (eventNumber) => {
     // Toggle the like status for the clicked event
@@ -25,58 +40,64 @@ function EventsList() {
     );
   };
 
-  const eventBoxStyles = (eventNumber) => ({
-    bgcolor: likedEvents.includes(eventNumber) ? '#ff5252' : 'transparent',
-    margin: '10px',
-    borderRadius: '16px',
-    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
-    position: 'relative',
-    width: 'calc(20% - 20px)',
-    cursor: 'pointer',
-    transition: 'background-color 0.3s ease-in-out',
-  });
-
-  const heartIconStyles = {
-    position: 'absolute',
-    top: '8px',
-    right: '8px',
-    cursor: 'pointer',
-    color: likedEvents.length > 0 ? 'white' : 'grey',
-    transition: 'color 0.3s ease-in-out',
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
-    <Paper
-      sx={{
-        width: '100%',
-        ml: 1,
-        mt: '3rem',
-        border: 0,
-        p: 2,
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
-      }}
-    >
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((el) => (
-        <Paper key={el} sx={eventBoxStyles(el)} onClick={() => handleLikeClick(el)}>
-          <IconButton sx={heartIconStyles}>
-            <FavoriteIcon />
-          </IconButton>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar>
-                <NavigateNextIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary={<Typography fontWeight="bold">Single-line text</Typography>}
-              secondary={el % 2 === 0 ? 'Secondary text' : 'Alternate text'}
-            />
-          </ListItem>
-        </Paper>
-      ))}
-    </Paper>
+    <div style={{ padding: '20px' }}>
+      <Slider {...settings}>
+        {events.map((event, index) => (
+
+          <div key={event.id} style={{ width: '100%', padding: '10px' }}> {/* Wrapper div for each slide */}
+            <Paper style={{ height: '200px', position: 'relative', padding: '20px',backgroundColor: '#FFFFFF' }}> {/* Relative position context */}
+              <IconButton onClick={() => handleLikeClick(event.id)} style={{ position: 'absolute', zIndex: 1 }}>
+                <FavoriteIcon />
+              </IconButton>
+              <div style={{ position: 'absolute', top: '10px', right: '10px', display: 'flex', alignItems: 'center' }}>
+                <CalendarTodayIcon style={{ marginRight: '5px' }} />
+                <Typography variant="body2">{event.date}</Typography>
+              </div>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <NavigateNextIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={<Typography fontWeight="bold">{event.title}</Typography>}
+                  secondary={
+                    Array.isArray(event.keywords) && event.keywords.map((keyword, keyIndex) => (
+                      <Chip key={keyIndex} label={keyword} style={{ margin: '5px', backgroundColor: '#cce7c9', color: 'black'}} />
+                    ))
+                  }
+                />
+              </ListItem>
+            </Paper>
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 }
 
--- src/views/OverviewPage.js
+++ src/views/OverviewPage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import EventsList from '../components/EventsList';
 import ChatAssistant from '../components/ChatAssistant';
 import EnvironmentalBadges from '../components/EnvironmentalBadges';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './OverviewPage.css'; // Make sure this is the correct path to your CSS file
 
 function OverviewPage() {
